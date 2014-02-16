1ec3cbc0e6c1f61c51408aac86fafe10bbf97704
Matt Wallington <matt@cpusage.com>
2014-02-16 01:17:12 -0800

    Added speedometer

---
+++ app/js/map.js
@@ -0,0 +1,47 @@
+  function getPoints(file, result) {
+      $.get(file, function(data) {
+        var points = data.split(';');
+        result(points)
+      });  
+    }
+    function plotPoints(file) {
+      getPoints(file, function(points){
+        var marker
+        var count = 0
+        for (key in points) {
+          arr = $.trim(points[key]).split(','); 
+          latlng = new google.maps.LatLng(arr[0], arr[1])
+          if (count == 0) {
+            marker = new google.maps.Marker({
+                position: latlng,
+                map: map,
+                title:"2001 BMW X5",
+                icon: "img/green_car.png"
+            });
+            // Click event
+            google.maps.event.addListener(marker, 'click', function() {
+              //Pop modal window.
+              $('#myModal').modal({
+                keyboard: true,
+                show: true
+              })
+            });
+          } else {
+            setTimeout(function(marker, latlng) {
+              marker.setPosition(latlng)
+            }, 3000 * count, marker, latlng)
+          }
+          count++;
+        }
+      });
+    }
+  
+    var map
+    function initialize() {
+      var mapOptions = {
+        center: new google.maps.LatLng(45.52, -122.6819),
+        zoom: 12
+      };
+      map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
